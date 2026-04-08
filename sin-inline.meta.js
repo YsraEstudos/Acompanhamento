4348542dc048d9c238a444b11ef49cf6d4811c6c
YsraEstudos <86720970097@ulife.com.br>
2026-04-08 11:29:58 -0300

    Release userscript metadata 1.0.3

---
+++ sin-inline.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         KM SIN Sidebar
+// @namespace    http://tampermonkey.net/
+// @version      1.0.3
+// @author       OpenAI Codex
+// @description  Exibe o Acompanhamento da SIN inline na página do item do Klassmatt.
+// @downloadURL  https://ysraestudos.github.io/km-sin-sidebar-userscript/releases/1.0.3/sin-inline.user.js
+// @updateURL    https://ysraestudos.github.io/km-sin-sidebar-userscript/sin-inline.meta.js
+// @match        https://*.klassmatt.com.br/*SIN_Item_Edita.aspx*
+// @match        https://*.klassmatt.com.br/*ITEM_Edita.aspx*
+// @match        https://klassmatt.com.br/*SIN_Item_Edita.aspx*
+// @match        https://klassmatt.com.br/*ITEM_Edita.aspx*
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        unsafeWindow
+// @run-at       document-end
+// ==/UserScript==
